be38b8bc370ed34cec47f03670f5d7d1fe33fb10
VHTORRES <victorhugobeaulieu@hotmail.com>
2021-09-07 15:26:02 -0300

    Basic static componentes

--- fronted/src/App.tsx
+++ fronted/src/App.tsx
@@ -1,8 +1,18 @@
+import DataTable from 'components/DataTable';
+import Footer from 'components/Footer';
+import Navbar from './components/Navbar';
+
 function App() {
   return (
-    <div >
-      <h1  className="text-primary">Olá Mundo</h1>
+    <>
+    <Navbar />
+    <div className="container" >
+      <h1 className="text-primary">Olá Mundo</h1>
+      
+      <DataTable/>
     </div>
+    <Footer/>
+   </>
   );
 }
 
