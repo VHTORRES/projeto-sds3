140e14cea74a31ba1a7d10f4c25f5fecccc0441c
VHTORRES <victorhugobeaulieu@hotmail.com>
2021-09-09 16:38:42 -0300

    First homolog

--- backend/src/main/java/com/devsuperior/dsvendas/config/SecurityConfig.java
+++ backend/src/main/java/com/devsuperior/dsvendas/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.devsuperior.dsvendas.config;
 
 import java.util.Arrays;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
