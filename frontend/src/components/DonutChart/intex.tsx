b064e993b91cc0a041f143bcc33769d3b32f6b84
VHTORRES <victorhugobeaulieu@hotmail.com>
2021-09-11 09:47:23 -0300

    BarChart integration

--- frontend/src/components/DonutChart/intex.tsx
+++ frontend/src/components/DonutChart/intex.tsx
@@ -24,7 +24,7 @@ const DonutChart = () => {
                 const mySeries = data.map(x => x.sum);
 
                 setChartData({ labels: myLabels, series: mySeries })
-                
+
             });
     }, []);
 
